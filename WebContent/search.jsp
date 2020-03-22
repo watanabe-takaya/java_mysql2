d55655556adecc0e983b6a585ab521eea5d18452
watanabe-takaya <watanabeta801@gmail.com>
2020-03-22 13:28:17 +0900

    初コミット

---
+++ WebContent/search.jsp
@@ -0,0 +1,58 @@
+<%@ page language="java" contentType="text/html;charset=Windows-31J"%>
+<%@page import="java.sql.*"%>
+
+<html>
+<head>
+<title></title>
+</head>
+<body>
+	<br>
+
+	<%
+		ResultSet rset = (ResultSet) request.getAttribute("kekka");
+	%>
+	<table border="1">
+		<tr bgcolor="#cccccc">
+			<td><b>ID</b></td>
+			<td><b>名前</b></td>
+			<td><b>性別</b></td>
+			<td><b>入社年</b></td>
+			<td><b>住所</b></td>
+			<td><b>削除</b></td>
+			<td><b>変更</b></td>
+		</tr>
+		<%
+			while (rset.next()) {
+		%>
+		<tr>
+			<td><%=rset.getString(1)%></td>
+			<td><%=rset.getString(2)%></td>
+			<td><%=rset.getString(3)%></td>
+			<td><%=rset.getString(4)%></td>
+			<td><%=rset.getString(5)%></td>
+<!---->
+			<td><a href="Edit?&mode=delete&id=<%=rset.getString(1)%>">削除</a></td>
+			<td>
+				<form action="/java_mysql/Edit" method="POST">
+					<input type="hidden" name="mode" value="change"> <input
+						type="hidden" name="id" value="<%=rset.getString(1)%>"> <input
+						type="hidden" name="name" value="<%=rset.getString(2)%>">
+					<input type="hidden" name="sei" value="<%=rset.getString(3)%>">
+					<input type="hidden" name="nen" value="<%=rset.getString(4)%>">
+					<input type="hidden" name="address" value="<%=rset.getString(5)%>">
+					<input type="submit" value="変更">
+				</form>
+			</td>
+
+		</tr>
+		<%
+			}
+		%>
+	</table>
+
+	<br>
+
+	<a href="./index.jsp">トップに戻る</a>
+
+</body>
+</html>
---
+++ src/info/searchman/lesson/java_mysql/EditServlet.java
@@ -0,0 +1,69 @@
+package info.searchman.lesson.java_mysql;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class EditServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		// 文字コードの設定
+		request.setCharacterEncoding("Windows-31J");
+
+		// modeの取得
+		String mode = request.getParameter("mode");
+
+		// 実行ステータスの宣言
+		String status = "成功しました";
+
+		// JavaBeansの初期化
+		ShainBeans shain = new ShainBeans(request);
+
+		switch (mode) {
+
+		case "add": // 登録
+			if (shain.addData() == false) {
+				status = "失敗しました";
+			}
+			break;
+
+		case "delete": // 削除
+			if (shain.deleteData() == false) {
+				status = "失敗しました";
+			}
+			break;
+
+		case "change": // 変更
+			request.setAttribute("shain", shain);
+			request.getRequestDispatcher("/change.jsp").forward(request, response);
+			return;
+
+		case "del_add": // 変更確定
+			if (!(shain.deleteData() && shain.addData())) {
+				status = "失敗しました";
+			}
+			break;
+		}
+
+		// statusをセットして、result.jspに転送
+		request.setAttribute("status", status);
+		request.getRequestDispatcher("/result.jsp").forward(request, response);
+
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		doPost(request, response);
+	}
+	
+}
